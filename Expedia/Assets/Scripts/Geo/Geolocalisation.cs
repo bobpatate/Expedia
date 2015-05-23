58795ea5f6cc399ebbe9053a5ccf463a88a3b49c
Gab <ghebert10@gmail.com>
2015-05-23 16:56:04 -0400

    geodistance

--- Expedia/Assets/Scripts/Geo/Geolocalisation.cs
+++ Expedia/Assets/Scripts/Geo/Geolocalisation.cs
@@ -45,22 +45,4 @@ public class Geolocalisation : MonoBehaviour
 		// Stop service if there is no need to query location updates continuously
 		Input.location.Stop();
 	}
-	 /*
-	public void distance()//float lat1, float lon1, float lat2, float lon2)
-	{
-		Debug.Log ("Dick");
-		float lat1 = 45.511969f;
-		float lon1 = -73.569880f;
-		
-		float lat2 = 45.5579957f;   //stade
-		float lon2 = -73.5518816f;  //stade
-
-		float R = 1.609344f;
-		float dlon = lon2 - lon1;
-		float dlat = lat2 - lat1; 
-		float a = Mathf.Pow(Mathf.Sin(dlat / 2), 2)+ Mathf.Cos(lat1) * Mathf.Cos(lat2) * Mathf.Pow(Mathf.Sin(dlon / 2),2);
-		float c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1-a) );
-		float d = R * c;
-		Debug.Log(d);
-	} */
 }
