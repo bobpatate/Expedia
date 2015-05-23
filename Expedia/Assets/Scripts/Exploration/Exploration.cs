199454d3d8b25b47c19504f320925a04ca4a6c04
Gab <ghebert10@gmail.com>
2015-05-23 15:45:32 -0400

    exploration data structure

---
+++ Expedia/Assets/Scripts/Exploration/Exploration.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+using SimpleJSON;
+using System.Text;
+
+public class Exploration : MonoBehaviour {
+	internal float radius = 0.5f;
+	internal float playerLat = 45.511463f;
+	internal float playerLng = -73.570863f;
+
+	public List<ExplorationPoint> points = new List<ExplorationPoint>(); //List of nearby points
+
+	private string url;
+
+	void Start() {
+		Actualize();
+	}
+
+	void Actualize(){
+		url = "http://terminal2.expedia.com:80/x/geo/features?within="+ radius +"km&lat="+ playerLat +"&lng="+ playerLng +"&type=point_of_interest&apikey=azCQTn91WiTmxxlaKWG63YZk2z1fpXxA";
+
+		WWW www = new WWW(url);
+		StartCoroutine(WaitForRequest(www));
+	}
+	
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+
+		if (www.error == null){
+			Debug.Log("WWW Ok!: " + www.text);
+
+			//Create and store nearby explorations points
+			var data = JSONNode.Parse(www.text);
+			for(int i=0; i<data.Count; i++){
+				//Debug.Log (data[i]["id"].GetType());
+				ExplorationPoint newExpP = new ExplorationPoint((uint)data[i]["id"].AsInt,
+				                                                (string)data[i]["type"],
+				                                                (string)data[i]["name"],
+				                                                (uint)data[i]["source"]["srcId"].AsInt,
+				                                                (uint)data[i]["source"]["systemId"].AsInt,
+				                                                (string) data[i]["position"]["type"],
+				                                                (double)data[i]["position"]["coordinates"][0].AsInt,
+				                                                (double)data[i]["position"]["coordinates"][1].AsInt,
+				                                                (string)data[i]["status"]);
+
+				points.Add(newExpP);
+			}
+		}
+		else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+}
---
+++ Expedia/Assets/Scripts/Exploration/ExplorationPoint.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ExplorationPoint {
+	public uint id {get; private set;}
+	public string type {get;private set;}
+	public string name {get; private set;}
+	public Source source {get; private set;}
+	public Position position {get; private set;}
+	public string status {get; private set;}
+
+	public ExplorationPoint(uint _id, string _type, string _name, uint _srcId, uint _systemId, string _posType, double _lat, double _lng, string _status){
+		id = _id;
+		type = _type;
+		name = _name;
+		Source _source = new Source(_srcId, _systemId);
+		source = _source;
+		Position _position = new Position(_posType,_lat, _lng);
+		position = _position;
+		status = _status;
+	}
+}
+
+public class Source{
+	public uint srcId {get; private set;}
+	public uint systemId {get; private set;}
+
+	public Source(uint _srcId, uint _systemId){
+		srcId = _srcId;
+		systemId = _systemId;
+	}
+}
+
+public class Position{
+	public string type {get; private set;}
+	public List<double> coordinates = new List<double>();
+
+	public Position(string _type, double _lat, double _lng){
+		type = _type;
+		coordinates.Add(_lat);
+		coordinates.Add(_lng);
+	}
+}
