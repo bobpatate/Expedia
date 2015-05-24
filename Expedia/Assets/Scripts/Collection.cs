77d97ccdf9216fe7f66097829403814d73b9fc61
Philippe Rheaume <trucphil85@hotmail.com>
2015-05-24 06:51:32 -0400

    Collection; fonction addtrophy

---
+++ Expedia/Assets/Scripts/Collection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collection : MonoBehaviour {
+	private int smalltrophy = 0;
+	private int largetrophy = 0;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void addSmallTrophy()
+	{
+		smalltrophy++;
+		PlayerPrefs.SetInt("STrophy", smalltrophy);
+	}
+
+	public void addLargeTrophy()
+	{
+		largetrophy++;
+		PlayerPrefs.SetInt("LTrophy", largetrophy);
+	}
+
+
+}
